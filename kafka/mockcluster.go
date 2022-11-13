644add27f84c110eca2c57b90a1e902dff40124d
kkoehler <github@kkoehler.com>
2022-11-13 18:06:42 +0100

    added SetBrokerDown and SetBrokerUp for MockCluster

--- kafka/mockcluster.go
+++ kafka/mockcluster.go
@@ -92,6 +92,26 @@ func (mc *MockCluster) SetRoundtripDuration(brokerId int, duration time.Duration
 	return nil
 }
 
+// SetBrokerDown disconnects the broker and disallows any new connections.
+// This does NOT trigger leader change.
+func (mc *MockCluster) SetBrokerDown(brokerId int) error {
+	cError := C.rd_kafka_mock_broker_set_down(mc.mcluster, C.int(brokerId))
+	if cError != C.RD_KAFKA_RESP_ERR_NO_ERROR {
+		return newError(cError)
+	}
+	return nil
+}
+
+// SetBrokerUp makes the broker accept connections again.
+// This does NOT trigger leader change.
+func (mc *MockCluster) SetBrokerUp(brokerId int) error {
+	cError := C.rd_kafka_mock_broker_set_up(mc.mcluster, C.int(brokerId))
+	if cError != C.RD_KAFKA_RESP_ERR_NO_ERROR {
+		return newError(cError)
+	}
+	return nil
+}
+
 // Close and destroy the MockCluster
 func (mc *MockCluster) Close() {
 	C.rd_kafka_mock_cluster_destroy(mc.mcluster)
